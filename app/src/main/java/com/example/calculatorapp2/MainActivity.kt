f44ddabc6a2e12d1e4191e43516f636cd4733cff
[ms] <[namohammedshakeel21@gmail.com]>
2023-08-30 21:16:27 +0530

    updating project

--- app/src/main/java/com/example/calculatorapp2/MainActivity.kt
+++ app/src/main/java/com/example/calculatorapp2/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.calculatorapp2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import com.example.calculatorapp2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var resulttv:TextView
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        resulttv = findViewById(R.id.resulttv)
+    }
+    fun onClickDigit(view: View){
+
+        if(binding.resulttv.text.toString() == "0"){
+            binding.resulttv.text = (view as Button).text
+        }else{
+            resulttv.append((view as Button).text)
+        }
+
+    }
+
+    fun onClickCancel(view: View){
+        binding.resulttv.text = binding.resulttv.text.toString().substring(0,binding.tvAnswer.text.length-1)
+        if (binding.resulttv.text.toString() == ""){
+            binding.resulttv.text = "0"
+        }
+    }
+
+    fun onClear(view: View){
+        binding.resulttv.text = "0"
     }
 }
